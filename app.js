e0f16fbf8e3a47bf053947d660584caa72439cfe
Stepan Dordiai <stepandordiai@gmail.com>
2025-10-07 15:08:07 +0200

    Add capitalizeEachWord function

--- app.js
+++ app.js
@@ -22,3 +22,16 @@ const handleReverse = () => {
 	const reversedTxt = displayValue.split("").reverse().join("");
 	display.value = reversedTxt;
 };
+
+const handleCapitalizeEachWord = () => {
+	const display = document.getElementById("display");
+	const displayValue = display.value;
+	const text = displayValue.split(" ");
+	let txt = [];
+	text.forEach((word) => {
+		const firstLetter = word.split("")[0].toUpperCase();
+		const restText = word.slice(1);
+		txt.push(firstLetter + restText);
+	});
+	display.value = txt.join(" ");
+};
